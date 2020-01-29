4eb59d89c72a6957e8de7a45f6124d3c80813ab7
Захаров Сергей Геннадьевич <ZakharovSG@global.bcs>
2020-01-29 13:43:18 +0300

    Updates

---
+++ precache-manifest.a83c68e2b4b67443774d3c79f0288709.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27debb6627f81e49bdebf23a3490f7af",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "db89b698c04a1b55c39b",
+    "url": "/react-portfolio/static/css/3.690434aa.chunk.css"
+  },
+  {
+    "revision": "c2549bf756973e3b6da9",
+    "url": "/react-portfolio/static/css/4.660f154d.chunk.css"
+  },
+  {
+    "revision": "84792eedd9c44eaa2b9d",
+    "url": "/react-portfolio/static/css/5.d4d5ce48.chunk.css"
+  },
+  {
+    "revision": "1fe465c239d79da0d988",
+    "url": "/react-portfolio/static/css/6.d92f729a.chunk.css"
+  },
+  {
+    "revision": "fd90491bbe9ab5168c17",
+    "url": "/react-portfolio/static/css/main.5b131f3d.chunk.css"
+  },
+  {
+    "revision": "58ed0f74be91d0303d26",
+    "url": "/react-portfolio/static/js/2.5ba9deb5.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-portfolio/static/js/2.5ba9deb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db89b698c04a1b55c39b",
+    "url": "/react-portfolio/static/js/3.a3dadeea.chunk.js"
+  },
+  {
+    "revision": "c2549bf756973e3b6da9",
+    "url": "/react-portfolio/static/js/4.e68f4b3f.chunk.js"
+  },
+  {
+    "revision": "84792eedd9c44eaa2b9d",
+    "url": "/react-portfolio/static/js/5.c217bfee.chunk.js"
+  },
+  {
+    "revision": "1fe465c239d79da0d988",
+    "url": "/react-portfolio/static/js/6.e7662182.chunk.js"
+  },
+  {
+    "revision": "27ca442672f99ae36ec6",
+    "url": "/react-portfolio/static/js/7.d01c3ca6.chunk.js"
+  },
+  {
+    "revision": "fd90491bbe9ab5168c17",
+    "url": "/react-portfolio/static/js/main.474ad5dc.chunk.js"
+  },
+  {
+    "revision": "237f18175e094ed12ff4",
+    "url": "/react-portfolio/static/js/runtime-main.a1f1605c.js"
+  },
+  {
+    "revision": "32a34161cf563340d408c3cf0564ea57",
+    "url": "/react-portfolio/static/media/Raleway-Bold.32a34161.woff"
+  },
+  {
+    "revision": "6d492c121a545d14bc89dc55c20c9330",
+    "url": "/react-portfolio/static/media/Raleway-Bold.6d492c12.woff2"
+  },
+  {
+    "revision": "b371f62e7f055ed663e08081be51cffd",
+    "url": "/react-portfolio/static/media/Raleway-Bold.b371f62e.ttf"
+  },
+  {
+    "revision": "c799c87b9b8e9a84ebc917a1ce8ae5a0",
+    "url": "/react-portfolio/static/media/Raleway-Bold.c799c87b.svg"
+  },
+  {
+    "revision": "f140be2476fe772b8070f2cb6373a4be",
+    "url": "/react-portfolio/static/media/Raleway-Bold.f140be24.eot"
+  },
+  {
+    "revision": "00fa035a7b805382cf69ca5d66b7bd7f",
+    "url": "/react-portfolio/static/media/Raleway-Light.00fa035a.eot"
+  },
+  {
+    "revision": "56e8eb7e2a6972b5352401c53c924c14",
+    "url": "/react-portfolio/static/media/Raleway-Light.56e8eb7e.woff2"
+  },
+  {
+    "revision": "b7630cc28fd6d4976c754ef9aafcc8cc",
+    "url": "/react-portfolio/static/media/Raleway-Light.b7630cc2.svg"
+  },
+  {
+    "revision": "dc5d4c98378b0df8497276ba4ae88249",
+    "url": "/react-portfolio/static/media/Raleway-Light.dc5d4c98.ttf"
+  },
+  {
+    "revision": "e61949d70d4dcf79714f7fcf3d83596b",
+    "url": "/react-portfolio/static/media/Raleway-Light.e61949d7.woff"
+  },
+  {
+    "revision": "23bf527c45ef7d75883dace273bcc11a",
+    "url": "/react-portfolio/static/media/Raleway-Medium.23bf527c.eot"
+  },
+  {
+    "revision": "4a2de5ced4622583bc232e1c01d6977e",
+    "url": "/react-portfolio/static/media/Raleway-Medium.4a2de5ce.woff2"
+  },
+  {
+    "revision": "8431aa148cc94aa3bd1f556ba23e6950",
+    "url": "/react-portfolio/static/media/Raleway-Medium.8431aa14.woff"
+  },
+  {
+    "revision": "97384a41aea00ee029788c03180bc003",
+    "url": "/react-portfolio/static/media/Raleway-Medium.97384a41.ttf"
+  },
+  {
+    "revision": "ea8f2d59cd3261f6d07f7ba1f44d9bb9",
+    "url": "/react-portfolio/static/media/Raleway-Medium.ea8f2d59.svg"
+  },
+  {
+    "revision": "28a806194d2aa1546e923d450eda213f",
+    "url": "/react-portfolio/static/media/Raleway-Regular.28a80619.eot"
+  },
+  {
+    "revision": "9ce4555c2a149073f0c8c235379b98ae",
+    "url": "/react-portfolio/static/media/Raleway-Regular.9ce4555c.svg"
+  },
+  {
+    "revision": "b34a2243cd07471ec55d4e9c133cd669",
+    "url": "/react-portfolio/static/media/Raleway-Regular.b34a2243.woff"
+  },
+  {
+    "revision": "f03b40626e11a6d3303d1cf17ee7411c",
+    "url": "/react-portfolio/static/media/Raleway-Regular.f03b4062.woff2"
+  },
+  {
+    "revision": "f7955b08758fc5a73568d9c2d8fd812c",
+    "url": "/react-portfolio/static/media/Raleway-Regular.f7955b08.ttf"
+  },
+  {
+    "revision": "4ec00b38b7458c97cebe0ce6693a99b8",
+    "url": "/react-portfolio/static/media/about.4ec00b38.svg"
+  },
+  {
+    "revision": "5b60252a4c8315f4336df9d214209d2e",
+    "url": "/react-portfolio/static/media/code.5b60252a.svg"
+  },
+  {
+    "revision": "8edf977eb011ed22272c8e88f0ca9572",
+    "url": "/react-portfolio/static/media/github-icon.8edf977e.svg"
+  },
+  {
+    "revision": "fb40e938c3434e7459675bc1b25b7deb",
+    "url": "/react-portfolio/static/media/guitar.fb40e938.svg"
+  },
+  {
+    "revision": "23f429552b1760ac913d237bcd034830",
+    "url": "/react-portfolio/static/media/instagram-icon.23f42955.svg"
+  },
+  {
+    "revision": "6c0916da35a767179eaaa198ed6da57a",
+    "url": "/react-portfolio/static/media/logo.6c0916da.svg"
+  },
+  {
+    "revision": "ee4e3f35bf80ba5f078c2cbfa77c4434",
+    "url": "/react-portfolio/static/media/sawing.ee4e3f35.svg"
+  },
+  {
+    "revision": "cb508ddfae03c5c5210dcf10648ddd0f",
+    "url": "/react-portfolio/static/media/superpower.cb508ddf.svg"
+  },
+  {
+    "revision": "0572b3b9e2716c989d15ea6727f3b344",
+    "url": "/react-portfolio/static/media/tutorial.0572b3b9.svg"
+  },
+  {
+    "revision": "46bc0054f4bb8f9bce195c740c1296d3",
+    "url": "/react-portfolio/static/media/vk-icon.46bc0054.svg"
+  }
+]);
